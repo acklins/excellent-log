229e2111880eafedb687f5d8db0c8c8ad45cc4a5
acklins <dmcmillan100@gmnail.com>
2021-03-20 22:05:45 -0700

    unit test of url path 'about' passed

--- main_app/tests.py
+++ main_app/tests.py
@@ -3,7 +3,6 @@ from django.shortcuts import reverse
 
 # Create your tests here.
 #checking that the url I defined in urls.py works
-
 class HomePageTests(SimpleTestCase):
     def test_home_url_name(self):
         response = self.client.get(reverse('about'))
